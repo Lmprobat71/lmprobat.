130ebe3d3d15c1a7f6b980595a9c2c489f8e4806
lmprobat71 <lmprobat@free.fr>
2024-12-08 21:15:01 +0100

    update_fichier_ntfly

--- .netlify/functions/gallery.js
+++ .netlify/functions/gallery.js
@@ -1,25 +1,21 @@
-const fs = require('fs').promises; // Utiliser les promesses
+const fs = require('fs').promises;
 const path = require('path');
 
 exports.handler = async function(event, context) {
     const service = event.queryStringParameters.service;
-    if (!service || !/^[a-zA-Z0-9-_]+$/.test(service)) { // Validation supplémentaire
+    if (!service || !/^[a-zA-Z0-9-_]+$/.test(service)) {
         return {
             statusCode: 400,
             body: JSON.stringify({ error: 'Service non spécifié ou invalide.' })
         };
     }
 
-    // Utilisation d'un chemin absolu
     const directory = path.join('/', 'images', 'Galery', service);
 
     try {
-        // Vérifier si le répertoire existe
         await fs.access(directory);
-        
-        // Lire les fichiers dans le répertoire
         const files = await fs.readdir(directory);
-        const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file)); // Sensibilité à la casse
+        const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
 
         if (!images.length) {
             return {
@@ -35,7 +31,7 @@ exports.handler = async function(event, context) {
     } catch (error) {
         return {
             statusCode: 404,
-            body: JSON.stringify({ error: `Répertoire non trouvé ou inaccessible: ${error.message}` }) // Message d'erreur détaillé
+            body: JSON.stringify({ error: `Répertoire non trouvé ou inaccessible: ${error.message}` })
         };
     }
 };
