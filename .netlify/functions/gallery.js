d9bed8859e10f3bf8b60694fd78fe895ec8c3def
lmprobat71 <lmprobat@free.fr>
2024-12-08 22:22:22 +0100

    ajou_scan_chemin

--- .netlify/functions/gallery.js
+++ .netlify/functions/gallery.js
@@ -1,6 +1,30 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+// Fonction pour vérifier les permissions d'un chemin
+async function checkPermissions(directory) {
+    try {
+        await fs.access(directory);
+        console.log(`Accès autorisé pour : ${directory}`);
+    } catch (error) {
+        console.log(`Accès refusé ou répertoire non trouvé pour : ${directory}`);
+    }
+}
+
+// Fonction pour scanner les chemins et vérifier les autorisations
+async function scanPaths(basePath, service) {
+    const directoriesToCheck = [
+        path.join(basePath),
+        path.join(basePath, 'images'),
+        path.join(basePath, 'images', 'Galery'),
+        path.join(basePath, 'images', 'Galery', service)
+    ];
+
+    for (const dir of directoriesToCheck) {
+        await checkPermissions(dir);
+    }
+}
+
 exports.handler = async function(event, context) {
     const service = event.queryStringParameters.service;
     if (!service || !/^[a-zA-Z0-9-_]+$/.test(service)) {
@@ -10,13 +34,10 @@ exports.handler = async function(event, context) {
         };
     }
 
-    // Log du service pour le débogage
-    console.log('Service:', service);
+    const basePath = path.join(__dirname, '..', '..');
+    await scanPaths(basePath, service);
 
-    const directory = path.join(__dirname, '..', '..', 'images', 'Galery', service);
-
-    // Log du répertoire pour le débogage
-    console.log('Directory:', directory);
+    const directory = path.join(basePath, 'images', 'Galery', service);
 
     try {
         await fs.access(directory);
@@ -25,8 +46,8 @@ exports.handler = async function(event, context) {
 
         if (!images.length) {
             return {
-                statusCode: 404,
-                body: JSON.stringify({ error: 'Aucune image trouvée pour ce service.' })
+            statusCode: 404,
+            body: JSON.stringify({ error: 'Aucune image trouvée pour ce service.' })
             };
         }
 
@@ -35,7 +56,6 @@ exports.handler = async function(event, context) {
             body: JSON.stringify(images.map(image => `/images/Galery/${service}/${image}`))
         };
     } catch (error) {
-        // Log de l'erreur pour le débogage
         console.error('Erreur:', error.message);
 
         return {
